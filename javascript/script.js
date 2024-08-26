79fe668e38d9bfbc36dd497f9ad0acf25e1bca39
Romain <romain.sanieres@gmail.com>
2024-08-26 08:30:40 +0200

    create getEvenNumbers function

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+// Array of numbers
+const numbers = [54, 65, 89, 46, 13, 76, 43];
+
+// This function filters the array to include only elements at even indices
+function getEvenNumbers(numbers) {
+  // index % 2 === 0 determines if the array index is even
+  const evenNumbers = numbers.filter((number, index) => index % 2 === 0);
+  console.log(evenNumbers);
+  // and return new array
+  return evenNumbers;
+}
+
+// Wait for the DOM to be fully loaded before executing the script
+document.addEventListener("DOMContentLoaded", function () {
+  // Call the functions
+  getEvenNumbers(numbers);
+});
