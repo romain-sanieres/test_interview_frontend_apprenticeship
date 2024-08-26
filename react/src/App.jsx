43977496063ac99b69faf5ad1c53721f837bcf3a
Romain <romain.sanieres@gmail.com>
2024-08-26 09:17:22 +0200

    add styles

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,6 +2,7 @@ import Counter from "../components/Counter";
 import Greeting from "../components/Greeting";
 import Header from "../components/Header";
 import "./App.css";
+
 const names = ["Alice", "Bob", "Charlie", "David"];
 
 function App() {
@@ -10,9 +11,12 @@ function App() {
       <Header />
       <main>
         <Counter />
-        {names.map((item, index) => (
-          <Greeting name={item} key={index} />
-        ))}
+        <p className="list-title">Names List</p>
+        <div className="greeting-list">
+          {names.map((item, index) => (
+            <Greeting name={item} key={index} />
+          ))}
+        </div>
       </main>
     </>
   );
