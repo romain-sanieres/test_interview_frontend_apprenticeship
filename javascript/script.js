69945447e5b744bedec04fa9903c0bfd66591d95
Romain <romain.sanieres@gmail.com>
2024-08-26 08:44:11 +0200

    init react project with vite

--- javascript/script.js
+++ javascript/script.js
@@ -10,19 +10,32 @@ function getEvenNumbers(numbers) {
   return evenNumbers;
 }
 
+// Create an async function to fetch and display todos
 async function fetchTodos() {
   try {
+    // Fetch data from the API and wait for the response
     const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+    // Convert the response to JSON
     const todos = await res.json();
+
+    // Extract the first 5 todos from the array
     const selectFiveTodos = todos.slice(0, 5);
+
+    // Select the ul element with id 'todoList'
     const todoList = document.getElementById("todoList");
 
+    // Iterate through each element in selectFiveTodos
     for (let i = 0; i < selectFiveTodos.length; i++) {
+      // Create a new li element
       const li = document.createElement("li");
+      // Set the text of the li element to the title of the current todo
       li.textContent = selectFiveTodos[i].title;
+      // Append the li element as a child to the ul element
       todoList.appendChild(li);
     }
   } catch (err) {
+    // log error if error
     console.log(err);
   }
 }
