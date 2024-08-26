250dc2a80848754194ae3ce0e4f477126dfc126e
Romain <romain.sanieres@gmail.com>
2024-08-26 08:37:11 +0200

    add fetchTodos function

--- javascript/script.js
+++ javascript/script.js
@@ -10,13 +10,31 @@ function getEvenNumbers(numbers) {
   return evenNumbers;
 }
 
+async function fetchTodos() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const todos = await res.json();
+    const selectFiveTodos = todos.slice(0, 5);
+    const todoList = document.getElementById("todoList");
+
+    for (let i = 0; i < selectFiveTodos.length; i++) {
+      const li = document.createElement("li");
+      li.textContent = selectFiveTodos[i].title;
+      todoList.appendChild(li);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 // Wait for the DOM to be fully loaded before executing the script
 document.addEventListener("DOMContentLoaded", function () {
   // Call the functions
   getEvenNumbers(numbers);
+  fetchTodos();
 
-   // Add event listener for the button
-   document.getElementById("changeText").addEventListener("click", function () {
+  // Add event listener for the button
+  document.getElementById("changeText").addEventListener("click", function () {
     document.getElementById("myText").textContent = "Hello, Check!";
   });
 });
